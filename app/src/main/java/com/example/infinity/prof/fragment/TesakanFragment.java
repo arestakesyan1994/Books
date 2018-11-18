9b3b190a3fa50c176a09c4d024d1abd014def3c9
arestakesyan1994 <arestakesyan1994@mail.ru>
2018-11-18 22:38:31 +0400

    my comment

--- app/src/main/java/com/example/infinity/prof/fragment/TesakanFragment.java
+++ app/src/main/java/com/example/infinity/prof/fragment/TesakanFragment.java
@@ -4,6 +4,7 @@ import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,13 +26,13 @@ import java.util.HashMap;
 import retrofit2.Call;
 import retrofit2.Callback;
 
-public class TesakanFragment extends Fragment {
+public class TesakanFragment extends Fragment implements View.OnClickListener {
     private ListView listView;
     ApiService mApiService;
     ResponseHandler session;
     Button php, html, jQuery, web, mvc, wordpress, js, laravel, angularJS,
             javaScript, nodeJs, c, wpf, sql, asp, oop, angular, react, froms, vue;
-
+    public static String KEY;
 
 //    String[] users = new String[]{"Ընդհանուր վեբ ծրագրավորում","HTML, CSS", "JavaScript, jQuery", "PHP, MySQL", "MVC, Codeigniter","Wordpress", "Laravel", "AngularJS","JS - ընդլայնված", "JS մասնագիտացված", "Node.js", "C#",
 //            "Windows Froms,WPF", "SQL Server,ADD.NET", "ASP.NET MVC", "ES6, OOP","Node.js", "Angular", "React.js","Vue.js", "Angular, React.js, Vue.js"};
@@ -55,28 +56,185 @@ public class TesakanFragment extends Fragment {
         session.checkLogin();
 
         php = tesakan.findViewById(R.id.php);
-        php.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(tesakan.getContext(),tasakanHome.class);
-                startActivity(intent);
-            }
-        });
+        php.setOnClickListener(this);
 
-//        HashMap<String, String> user = session.getResponseDetails();
-//        String moduleName = user.get(ResponseHandler.MODULES_NAME);
-//        String[] mName = moduleName.split(";");
-//        System.out.println(mName);
-//
-//        ArrayAdapter<CharSequence> end = new ArrayAdapter<CharSequence>(tesakan.getContext(), R.layout.thory_list,
-//                 users);
-//        listView = (ListView) tesakan.findViewById(R.id.theory);
-//        listView.setAdapter(end);
-//        theory();
+        html = tesakan.findViewById(R.id.html);
+        html.setOnClickListener(this);
+
+        jQuery = tesakan.findViewById(R.id.jQuery);
+        jQuery.setOnClickListener(this);
+
+        web = tesakan.findViewById(R.id.web);
+        web.setOnClickListener(this);
+
+        mvc = tesakan.findViewById(R.id.mvc);
+        mvc.setOnClickListener(this);
+
+        wordpress = tesakan.findViewById(R.id.wordpress);
+        wordpress.setOnClickListener(this);
+
+        js = tesakan.findViewById(R.id.js);
+        js.setOnClickListener(this);
+
+        laravel = tesakan.findViewById(R.id.laravel);
+        laravel.setOnClickListener(this);
+
+        angularJS = tesakan.findViewById(R.id.angularJS);
+        angularJS.setOnClickListener(this);
+
+        javaScript = tesakan.findViewById(R.id.javaScript);
+        javaScript.setOnClickListener(this);
+
+        nodeJs = tesakan.findViewById(R.id.nodeJs);
+        nodeJs.setOnClickListener(this);
+
+        c = tesakan.findViewById(R.id.c);
+        c.setOnClickListener(this);
+
+        wpf = tesakan.findViewById(R.id.wpf);
+        wpf.setOnClickListener(this);
+
+        sql = tesakan.findViewById(R.id.sql);
+        sql.setOnClickListener(this);
+
+        asp = tesakan.findViewById(R.id.asp);
+        asp.setOnClickListener(this);
+
+        oop = tesakan.findViewById(R.id.oop);
+        oop.setOnClickListener(this);
+
+        angular = tesakan.findViewById(R.id.angular);
+        angular.setOnClickListener(this);
+
+        react = tesakan.findViewById(R.id.react);
+        react.setOnClickListener(this);
+
+        froms = tesakan.findViewById(R.id.froms);
+        froms.setOnClickListener(this);
+
+        vue = tesakan.findViewById(R.id.vue);
+        vue.setOnClickListener(this);
+
         return tesakan;
     }
 
+    @Override
+    public void onClick(View v) {
+        Fragment fragment;
+        switch (v.getId()) {
+            case R.id.web:
+                KEY = "-1";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.html:
+                KEY = "1";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.jQuery:
+                KEY = "2";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.php:
+                KEY = "3";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.mvc:
+                KEY = "4";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.wordpress:
+                KEY = "5";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.laravel:
+                KEY = "6";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.angularJS:
+                KEY ="7";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.js:
+                KEY = "13";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.javaScript:
+                KEY = "18";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.nodeJs:
+                KEY ="8";
+                KEY="20";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.c:
+                KEY = "9";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.wpf:
+                KEY = "10";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.sql:
+                KEY = "11";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.asp:
+                KEY ="12";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.oop:
+                KEY = "19";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.angular:
+                KEY = "21";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.react:
+                KEY = "22";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.vue:
+                KEY = "23";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
+            case R.id.froms:
+                KEY = "24";
+                fragment = new TesakanHomeFragment();
+                displaySelectedFragment(fragment);
+                break;
 
+        }
+    }
+
+    private void displaySelectedFragment(Fragment fragment) {
+        Bundle bundle = new Bundle();
+        bundle.putString("Key", KEY);
+        fragment.setArguments(bundle);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
+    }
 
 //    private void theory() {
 //        mApiService.theory().enqueue(new Callback<Response>() {
--- app/src/main/java/com/example/infinity/prof/fragment/TesakanHomeFragment.java
+++ app/src/main/java/com/example/infinity/prof/fragment/TesakanHomeFragment.java
@@ -7,42 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.infinity.prof.R;
+import com.example.infinity.prof.constants.NavigationDrawerConstants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static com.example.infinity.prof.fragment.TesakanFragment.KEY;
 
 public class TesakanHomeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
+    TextView textJson;
+    String[] harcs=new String[]{};
+    ArrayList<String> h = new ArrayList<>();
+    ListView harcerList;
     public TesakanHomeFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static TesakanHomeFragment newInstance(String param1, String param2) {
-        TesakanHomeFragment fragment = new TesakanHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
+      @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+          super.onCreate(savedInstanceState);
+          getActivity().setTitle(NavigationDrawerConstants.TAG_TESAKAN);
     }
 
     @Override
@@ -51,37 +43,62 @@ public class TesakanHomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View tesakanHome = inflater.inflate(R.layout.fragment_tesakan_home, container, false);
 
-        int a = getArguments().getInt("inch_vor_key");
-        System.out.println(a);
+        textJson = (TextView)  tesakanHome.findViewById(R.id.textJson);
+        String json = "[{\"id\": 3, \"harc\": \"Տվյալների ի՞նչ տիպեր կան JS -ում\", \"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">\u200B<strong>JavaScript ծրագրավորման լեզվում առկա են տվյալների հետևյալ հիմնական տիպերը.</strong></span><br></p><ol><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Number</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">String</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Boolean</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Undefined</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Null</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Object</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Array</span></p></li></ol>\",\"module_id\": \"4\", \"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"}," +
+                "{ \"id\": 4,\"harc\": \"Որո՞նք են տվյալների ձևափոխման մեթոդները JS -ում\",\"patasxan\": \"<ul><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Y Տողային տիպից անցում ամբողջ թվային տիպ &nbsp; x = parseInt(y)</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Y Տողային տիպից անցում իրական թվային տիպ &nbsp; x = parseFloat(y)</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Y Տողային տիպից անցում ընդհանուր թվային տիպ &nbsp; x = Number(y)</span></p></li><li dir=\\\"ltr\\\"><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Թվային տիպից անցում տողային տիպ &nbsp;x = y.toString(); կամ x = y + “”;</span></p></li></ul>\",\n\"module_id\": \"2\",\"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"},{\"id\": 5,\"harc\": \"Ի՞նչ իմաստ ունի Undefined -ը JS -ում\",\"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Undefined -ը արժեք չունեցող կամ չհայտարարված փոփոխականի տիպն է: Օրինակ</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">var &nbsp;x;</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">console.log(x) // կտպի Undefined</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">console.log(y); // կտպի error, քանի որ չկա y փոփոխական</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">console.log(typeof y) &nbsp;կտպի undefined </span></p>\",\n\"module_id\": \"2\",\"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"},{ \"id\": 6,\"harc\": \"Լոկալ և գլոբալ փոփոխականներ\", \"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Լոկալ փոփոխականը ֆունկցիայի ներսում հայտարարված փոփոխականն է: Գլոբալ փոփոխականը ֆունկցիայից դուրս հայտարարված փոփոխականն է, որը տեսանելի կարող է լիենլ այդ namespace -ի մյուս ֆունկցիաներին:</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">EcmaScript6 -ում գոյություն ունեն բլոկային և ոչ բլոկային փոփոխականներ, օրինակ՝&nbsp;</span><span style=\\\"font-size: 14px;\\\"><br></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>if( 5 &gt; 4){</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>&nbsp; &nbsp;var x = 4;<br>}</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">console.log(x) //կտպի 4</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">var-ով հայտարարված փոփոխականները տեսանելի են բլոկից դուրս:</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>if( 5 &gt; 4){</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>&nbsp; &nbsp;let x = 4;<br>}</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(x)</strong> // error</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Let -ով հայտարարված փոփոխականները տեսանելի են միայն բլոկի ներսում:</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Let -ի և var -ի մեկ այլ տարբերություն է այն, որ var -ի օգնությամբ հնարավոր է հայտարարել նույն անունը ունեցող երկու փոփոխական;&nbsp;</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">օրինակ՝ var x = 4; var x = 8; let -ի դեպքում դա կհանգեցնի Error -ի :</span></p>\",\n\"module_id\": \"2\",\"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"},{ \"id\": 7, \"harc\": \"Բացատրել instanceof օպերատորի նշանակությունը\", \"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">JS -ի օբյեկտ կողմնորոշված կառուցվածքում բոլոր կլասները, այդ թվում String, Array, ժառանգվում են գլխավոր Object կլասից: &nbsp;Ենթադրենք ունենք հետևյալ կոդը</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>var x = [ 4, &nbsp;5, &nbsp;6 ];</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>var y = { x: 4, y: 6};</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(typeof x)</strong>; // Object</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(typeof y);</strong> //Object</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Երկու դեպքում էլ ստանում ենք նույն արդյունքը, սակայն x փոփոխականն իրականում զանգված է: instanceof օպերատորն օգնում է կոնկրետացնել տիպը.</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(x instanceof Array</strong>) //true</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(x instanceof Object)</strong> //true</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log( y instanceof Object</strong>) //true</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(y instanceof Array)</strong> //false =&gt; քանի որ y -ը զանգված չէ</span></p>\",\n" +
+                "\"module_id\": \"2\",\"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"}," +
+                "{\"id\": 8,\"harc\": \"Բացատրել in օպերատորի նշանակությունը\",\"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">In օպերատորը կարող է կիրառվել երկու նպատակով:</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Առաջինը ցիկլերում:</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Ենթադրենք<strong> var obj = {name: “Armen”, surname: “Davtyan”};</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>for(var key in obj){</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>&nbsp; &nbsp; &nbsp;console.log(key);</strong> //կտպի name,surname</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>&nbsp; &nbsp; &nbsp;console.log(obj[key]) </strong>// կտպի Armen, Davtyan</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>}</strong></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Երկրորդ դեպքում կարող ենք ստուգել՝ արդյո՞ք տվյալ օբյեկտում կա համապատասխան դաշտը</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Օրինակ&nbsp;</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>console.log(“name” in obj) //true</strong></span></p><p dir=\\\"ltr\\\"><strong><span style=\\\"font-size: 14px;\\\">if(“age” in obj){</span></strong></p><p><strong><span style=\\\"font-size: 14px;\\\">console.log(obj.age)</span></strong></p><p><span style=\\\"font-size: 14px;\\\"><strong>}</strong></span></p>\",\n\"module_id\": \"2\",\"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"}," +
+                "{ \"id\": 9,\"harc\": \"Բացատրել eval ֆունկցիան\", \"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Eval ֆունկցիան կարողանում է վերածել string -ը տրամաբանական արտահայտության և հաշվել դրա արժեքը:</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Օրինակ՝ &nbsp;</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>var y = eval(“4+5”); </strong>//9</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>eval(\\\"4+Math.sqrt(49)\\\")</strong> // 11</span></p>\",\n\"module_id\": \"2\", \"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"},{\"id\": 10, \"harc\": \"Ի՞նչ է տերնար օպերատորը\",\n\"patasxan\": \"<p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Տերնար օպերատորը պայմանական օպերատորի կարճ գրելաձևն է</span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>( պայման ) ? true : false</strong></span><span style=\\\"font-size: 14px;\\\"><br></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Օրինակ՝&nbsp;</span><span style=\\\"font-size: 14px;\\\"><br></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>If(a &gt; b){</strong></span></p><p dir=\\\"ltr\\\"><strong><span style=\\\"font-size: 14px;\\\">&nbsp; &nbsp; max =a</span></strong></p><p dir=\\\"ltr\\\"><strong><span style=\\\"font-size: 14px;\\\">}</span></strong></p><p dir=\\\"ltr\\\"><strong><span style=\\\"font-size: 14px;\\\">else{</span></strong></p><p dir=\\\"ltr\\\"><strong><span style=\\\"font-size: 14px;\\\">&nbsp; &nbsp;max = b;</span></strong></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>}</strong></span><span style=\\\"font-size: 14px;\\\"><br></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\">Կարճ գրելաձևը կլինի</span><span style=\\\"font-size: 14px;\\\"><br></span></p><p dir=\\\"ltr\\\"><span style=\\\"font-size: 14px;\\\"><strong>var max = (a &gt; b ) ? a : b</strong></span></p>\",\n\"module_id\": \"3\",\"author\": \"Ուսումնական կենտրոնի ղեկավար, Արմեն Մարջինյան\"}]";
+        String data = "";
+        String harcer="";
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            String myInt = bundle.getString("Key", KEY);
+            System.out.println(myInt);
+            try {
+                JSONArray jsonRootObject = new JSONArray(json);
+
+                for(int i=0; i < jsonRootObject.length(); i++) {
+                    JSONObject jsonObject = jsonRootObject.getJSONObject(i);
+
+                    int id = Integer.parseInt(jsonObject.optString("id"));
+                    String harc = jsonObject.optString("harc");
+                    String patasxan = jsonObject.optString("patasxan");
+                    String moduleId = jsonObject.optString("module_id");
+                    String author = jsonObject.optString("author");
+
+                    String p1 =patasxan.replaceAll("<br>","\n");
+                    String p2 =p1.replaceAll("</li>","");
+                    String p3 =p2.replaceAll("<p dir=\"ltr\">","\n");
+                    String p4 =p3.replaceAll("<span style=\"font-size: 14px;\">\u200B","");
+                    String p5 =p4.replaceAll("<strong>"," ");
+                    String p6 =p5.replaceAll("</strong>"," ");
+                    String p7 =p6.replaceAll("</span>","");
+                    String p8 =p7.replaceAll("</p>","");
+                    String p9 =p8.replaceAll("<ol>","");
+                    String p10 =p9.replaceAll("<li dir=\"ltr\">","");
+                    String p11 =p10.replaceAll("</ol>","");
+                    String p12 =p11.replaceAll("<ul>","");
+                    String p13 =p12.replaceAll("</ul>","");
+                    String p14 =p13.replaceAll("<span style=\"font-size: 14px;\">","");
+                    String p15 =p14.replaceAll("&nbsp;","");
+                    String p16 =p15.replaceAll("&gt;",">");
+                    if (moduleId.equals(myInt)){
+                        harcer += harc+ "\n";
+                        h.add(harcer.toString());
+                    }
+                    data += "Node" + i + " : \n id= " + id + " \n Harc= " + harc + " \n Patasxan= " + p16 + " \n Modul Id" + moduleId + "\n Author" + author + "\n";
+                }
+            } catch (JSONException e) {e.printStackTrace();}
+//            System.out.println(data);
+            ArrayAdapter<CharSequence> end = new ArrayAdapter<CharSequence>(tesakanHome.getContext(), R.layout.row_items,harcs);
+            harcerList = (ListView) tesakanHome.findViewById(R.id.harcerList);
+            harcerList.setAdapter(end);
+            System.out.println(h);
+            textJson.setText(harcer);
+//        textJson.setText(data);
+        }
         return tesakanHome;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
